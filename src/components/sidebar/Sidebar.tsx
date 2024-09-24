e38cd08d85c603dde78684c1eece0912d51194d6
reza18s <swary2021@gmail.com>
2024-09-24 16:31:31 +0330

    fix

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -49,7 +49,7 @@ export default function Sidebar() {
           <div className={`${openSidebar ? "" : "hidden"} my-5`}>
             <Accordion type="single" collapsible>
               {points.map((point) => (
-                <AccordionItem key={point._id} value={point.name}>
+                <AccordionItem key={point._id} value={point._id}>
                   <AccordionTrigger className="my-1 h-10 rounded-xl bg-slate-100">
                     <div className="flex flex-row ">
                       <div className="ml-2 text-sm font-medium">
@@ -95,7 +95,7 @@ export default function Sidebar() {
                           viewBox="0 0 100 100"
                           role="img"
                           preserveAspectRatio="xMidYMid meet"
-                          className={`rotate-[${point.dQuality}deg]`}
+                          style={{ rotate: `${point.dAzimuth}deg` }}
                         >
                           <path
                             d="M50.03 5a2.516 2.516 0 0 0-2.43 1.76L34.493 48.548a2.51 2.51 0 0 0-.372 1.454c-.026.51.104 1.017.372 1.452l13.105 41.782c.737 2.352 4.065 2.352 4.802 0l13.105-41.785c.27-.436.399-.945.372-1.456a2.513 2.513 0 0 0-.372-1.45L52.401 6.76A2.513 2.513 0 0 0 50.03 5zM39.403 50.288h6.205c.152 2.306 2.048 4.134 4.392 4.134c2.344 0 4.24-1.828 4.392-4.134h6.461L50 84.078z"
